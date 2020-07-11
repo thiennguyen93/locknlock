c89d62f2a93baa66814cdee3831b6cdf164d55d5
= <=>
2020-07-11 10:24:24 +0700

    ok

--- view/product_detail.view.php
+++ view/product_detail.view.php
@@ -1,16 +1,57 @@
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-  <li class="nav-item">
-    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-  </li>
-</ul>
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+<div id="header-breadcrumbs" class="border-top navbreadcrumbs-mini">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white container py-1">
+        <ol class="breadcrumb py-1 my-0">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Library</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Data</li>
+        </ol>
+    </nav>
+</div>
+<div class="container">
+    <div id="product-detail">
+        <div class="product-title">
+            <h6>LOCK&LOCK</h6>
+            <h5>Bộ Túi Hộp Cơm Thủy Tinh Lock&Lock 4EA LLG224I3 (750ml)</h5>
+            <p>LLG224I3</p>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <img class='product-detail-img' src="http://www.locknlock.vn/data/base/goods/big/201814150559907.jpg" alt="" srcset="">
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="row">
+                    <div class="col-3 pt-3">Giá bán</div>
+                    <div class="col-9 product-detail-price">697,000đ</div>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-3">Số lượng</div>
+                    <div class="col-9 ">
+                        <div class="form-group form-inline">
+                            <label for=""></label>
+                            <input type="number" class="form-control" name="" id="" aria-describedby="helpId" placeholder="Nhập số lượng cần mua" value="1">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="product-detail-content">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Thông tin sản phẩm </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Nhận xét</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Chính sách đổi trả</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+        </div>
+    </div>
+
 </div>
