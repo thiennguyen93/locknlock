74a72e5c302fad764216f996e0bd4cfa2a602027
= <=>
2020-07-11 20:13:53 +0700

    ok

--- controller/contact.controller.php
+++ controller/contact.controller.php
@@ -2,6 +2,9 @@
 include './model/contact.model.php';
 class controller {
     public function default() {
+        if (!isset($_POST)) {
+            
+        }
           return [
               'view' => 'contact'
           ];
--- view/layout/top_user_area.php
+++ view/layout/top_user_area.php
@@ -7,7 +7,7 @@
         <a class="nav-link" href="login.php">Đăng nhập</a>
         <a class="nav-link" href="register.php">Gia nhập thành viên</a>
     <?php } else { ?>
-        <a class="nav-link" href="user.php">Xin chào <?=$_SESSION['user_info']['hoten'] ?></a>
+        <a class="nav-link" href="user.php">Xin chào, <strong><?=$_SESSION['user_info']['hoten'] ?></strong></a>
         <a class="nav-link" href="logout.php">Đăng xuất</a>
     <?php } ?>
     <a class="nav-link" href="#">Chăm sóc cách hàng</a>
